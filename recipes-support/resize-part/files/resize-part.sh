e3b2ada57e34f2333f242c6b972efe25575e5ede
Ayoub Zaki <ayoub.zaki@embetrix.com>
2025-01-28 14:56:54 +0100

    resize-part: rework resize of partition

--- recipes-support/resize-part/files/resize-part.sh
+++ recipes-support/resize-part/files/resize-part.sh
@@ -3,9 +3,31 @@
 #
 
 LABEL="bitcoin"
-DEVICE=$(blkid --label $LABEL)
-if [ -z "$DEVICE" ]; then
+PART=$(blkid --label $LABEL)
+PART_NBR=${PART##*p}
+DEVICE="/dev/$(lsblk -no PKNAME $PART)"
+SECTORS=$(blockdev --getsz $PART)
+MIN_SEC=8388608   # 4GB in sectors
+MESSAGE=""
+
+if [ -z "$DEVICE" ] || [ -z "$PART" ] || [ -z "$PART_NBR" ]; then
     echo "No device found with label $LABEL"
+    MESSAGE="No device found with label $LABEL!"
+else
+    if [ $SECTORS -lt $MIN_SEC ]; then
+        echo "Resizing partition $PART"
+        if grep -q "^$PART" /proc/mounts; then
+            umount $PART
+        fi
+        sgdisk -d $PART_NBR -n $PART_NBR:0:0 -c $PART_NBR:$LABEL $DEVICE
+        partprobe $DEVICE
+        mkfs.ext4 -F $PART -L $LABEL
+        mount -L $LABEL
+        MESSAGE="Resize completed successfully."
+    else
+        MESSAGE="No need to resize partition."
+    fi
 fi
-systemd-notify --status="Resize completed successfully." --ready
-echo "done"
+
+systemd-notify --status=$MESSAGE --ready
+echo "done."
