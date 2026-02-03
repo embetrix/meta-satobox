229931339c4bf74ada0cf69054d1b92e846053b5
Ayoub Zaki <ayoub.zaki@embetrix.com>
2026-02-03 19:09:24 +0100

    wic: fitimage plugin  fix so that kernel partition is populated from a partition-specific staging dir

--- meta-satobox/scripts/lib/wic/plugins/source/fitimage.py
+++ meta-satobox/scripts/lib/wic/plugins/source/fitimage.py
@@ -21,9 +21,19 @@ class FitImage(SourcePlugin):
     name = 'fitimage'
 
     @classmethod
-    def do_prepare_partition(cls, part, source_params, creator, cr_workdir, oe_builddir, bootimg_dir, kernel_dir, native_sysroot, deploy_dir):
+    def do_prepare_partition(cls, part, source_params, creator, cr_workdir,
+                             oe_builddir, bootimg_dir, kernel_dir, rootfs_dir,
+                             native_sysroot):
         fitimage_path = os.path.join(kernel_dir, 'fitImage')
         if not os.path.exists(fitimage_path):
             raise RuntimeError('fitImage not found in: %s' % fitimage_path)
-        shutil.copy(fitimage_path, cr_workdir)
-        logger.info('fitImage copied to partition workdir: %s' % cr_workdir)
+
+        hdddir = "%s/fitimage.%d" % (cr_workdir, part.lineno)
+        if os.path.isdir(hdddir):
+            shutil.rmtree(hdddir)
+        os.makedirs(hdddir, exist_ok=True)
+
+        shutil.copy2(fitimage_path, os.path.join(hdddir, 'fitImage'), follow_symlinks=True)
+
+        part.prepare_rootfs(cr_workdir, oe_builddir, hdddir, native_sysroot, False)
+        logger.info('Prepared %s with only fitImage (%s)', part.label, fitimage_path)
